2a484d85d65dd5ecd18bf83929b286d4b5f8e916
jorgebg12 <jorgebartol12@gmail.com>
2024-12-02 00:27:30 +0100

    navbar buttons

--- src/app/Techniques/components/navbar/navbar.component.ts
+++ src/app/Techniques/components/navbar/navbar.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  @Output() changeTechniques = new EventEmitter<string>();
+  constructor() {}
+
+  OnClickAtack(): void {
+    this.changeTechniques.emit('Atack');
+  }
+  OnClickDefense(): void {
+    this.changeTechniques.emit('Defense');
+  }
+  OnClickPosition(): void {
+    this.changeTechniques.emit('Position');
+  }
+  OnClickPum(): void {
+    this.changeTechniques.emit('Pum');
+  }
+}
--- src/app/Techniques/components/technique-list/technique-list.component.ts
+++ src/app/Techniques/components/technique-list/technique-list.component.ts
@@ -13,8 +13,46 @@ export class TechniqueListComponent implements OnInit {
   constructor(private techniqueService: TechniquesService) {}
 
   ngOnInit(): void {
+    this.LoadAtacks();
+  }
+
+  TechniqueHandler(type: string) {
+    if (type === 'Atack') {
+      this.LoadAtacks();
+      return;
+    } else if (type === 'Defense') {
+      this.LoadDefenses();
+      return;
+    } else if (type === 'Position') {
+      this.LoadPositions();
+      return;
+    } else if (type === 'Pum') {
+      this.LoadPum();
+      return;
+    }
+  }
+
+  LoadAtacks(): void {
     this.techniqueService
       .getTechniquesAtack()
+      .subscribe((items: TechniqueDTO[]) => (this.techniqueList = items));
+  }
+
+  LoadDefenses(): void {
+    this.techniqueService
+      .getTechniquesDefense()
+      .subscribe((items: TechniqueDTO[]) => (this.techniqueList = items));
+  }
+
+  LoadPositions(): void {
+    this.techniqueService
+      .getTechniquesPosition()
+      .subscribe((items: TechniqueDTO[]) => (this.techniqueList = items));
+  }
+
+  LoadPum(): void {
+    this.techniqueService
+      .getTechniquesPum()
       .subscribe((items: TechniqueDTO[]) => (this.techniqueList = items));
   }
 }
